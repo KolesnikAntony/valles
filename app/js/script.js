3ad421e5423687fb6209b5478688d15f8a963c6c
KolesnikAntony <50579516+KolesnikAntony@users.noreply.github.com>
2019-12-02 19:34:29 +0100

    Final

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,7 @@
 $().ready(function() {
-
+    $('.service__title').on('click', (ev) => {
+        $(ev.currentTarget).toggleClass('service__title--active');
+    });
 });
 var swiper1 = new Swiper('.s1', { // Optional parameters
     direction: 'horizontal',
@@ -37,3 +39,58 @@ var swiper4 = new Swiper('.s4', {
     loop: true,
     width: 350,
 })
+var swiper5 = new Swiper('.s5', {
+    direction: 'horizontal',
+    loop: true,
+})
+var swiper6 = new Swiper('.s6', {
+    direction: 'horizontal',
+    loop: true,
+    width: 250,
+})
+
+window.onscroll = function() {
+    var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrolled >= 80)
+        document.querySelector(".topdesk").style.display = 'none';
+}
+
+
+let scrolled;
+let timer;
+let btn = document.querySelector('.btntop');
+
+btn.onclick = function() {
+    scrolled = window.pageYOffset;
+    scrollTop();
+
+}
+
+function scrollTop() {
+    if (scrolled > 0) {
+        window.scrollTo(0, scrolled);
+        scrolled -= 200; //Speed scroll
+        timer = setTimeout(scrollTop, 30);
+    } else {
+        clearTimeout(scrollTop);
+        window.scrollTo(0, 0);
+        document.querySelector(".topdesk").style.display = 'block';
+    }
+}
+
+
+let phone = document.querySelector('.phone');
+let table = document.querySelector('.phone__window');
+
+phone.onclick = function() {
+    table.classList.add('phone__window--active');
+}
+
+document.addEventListener('click', function(event) {
+    var isClickInside = phone.contains(event.target) || event.target === table;
+    console.log(isClickInside);
+
+    if (!isClickInside) {
+        table.classList.remove('phone__window--active');
+    }
+});
